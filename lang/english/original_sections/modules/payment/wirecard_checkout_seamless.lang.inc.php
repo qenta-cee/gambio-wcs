6dc14609aca0acc0397b795df47f8ae1b6340be7
Jakub Polomsky <jakub.polomsky@qenta.com>
2021-01-18 13:01:09 +0100

    update license headers

--- lang/english/original_sections/modules/payment/wirecard_checkout_seamless.lang.inc.php
+++ lang/english/original_sections/modules/payment/wirecard_checkout_seamless.lang.inc.php
@@ -1,26 +1,11 @@
 <?php
 /**
- * Shop System Plugins - Terms of use
- *
- * This terms of use regulates warranty and liability between Wirecard
- * Central Eastern Europe (subsequently referred to as WDCEE) and it's
- * contractual partners (subsequently referred to as customer or customers)
- * which are related to the use of plugins provided by WDCEE.
- *
- * The Plugin is provided by WDCEE free of charge for it's customers and
- * must be used for the purpose of WDCEE's payment platform integration
- * only. It explicitly is not part of the general contract between WDCEE
- * and it's customer. The plugin has successfully been tested under
- * specific circumstances which are defined as the shopsystem's standard
- * configuration (vendor's delivery state). The Customer is responsible for
- * testing the plugin's functionality before putting it into production
- * enviroment.
- * The customer uses the plugin at own risk. WDCEE does not guarantee it's
- * full functionality neither does WDCEE assume liability for any
- * disadvantage related to the use of this plugin. By installing the plugin
- * into the shopsystem the customer agrees to the terms of use. Please do
- * not use this plugin if you do not agree to the terms of use!
- */
+ * Shop System Plugins
+ * - Terms of use can be found under
+ * https://guides.qenta.com/shop_plugins:info
+ * - License can be found under:
+ * https://github.com/qenta-cee/gambio-qcs/blob/master/LICENSE
+*/
 
 $t_language_text_section_content_array = array(
 	'about'                           => '<br/><img src="images/wirecard-logo.png" alt="Wirecard CEE" /><br /><h3>Wirecard CEE - Your Full Service Payment Provider - Comprehensive solutions from one single source</h3>Wirecard AG is one of the world´s leading providers of outsourcing and white label solutions for electronic payment transactions.<br /><br /> As independent provider of payment solutions, we accompany our customers along the entire business development. Our payment solutions are perfectly tailored to suit e-Commerce requirements and have made us Austria´s leading payment service provider. Customization, competence, and commitment. <br /><br /> <a href="https://www.wirecard.at/" target="_blank">www.wirecard.at</a><br /><br />',
